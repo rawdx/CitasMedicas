731608606f7534effd2bf829458935f13f6c1dad
DESKTOP-USHSCCC\csi-r <csi22-rcoriaz@colegioaltair.net>
2023-04-13 14:39:59 +0200

    no message

--- GestionCitasMedicas/src/main/webapp/script/funciones.js
+++ GestionCitasMedicas/src/main/webapp/script/funciones.js
@@ -1,3 +1,43 @@
+function prueba() {
+	document.getElementById('textoUsuario').innerHTML = document.cookie
+
+
+	const nombres = ["Carlos", "Ana", "Miguel", "Lucía", "Pedro", "María", "Jorge", "Isabel"];
+	const apellidos = ["González", "Pérez", "García", "López", "Hernández", "Martínez", "Sánchez", "Fernández"];
+	const licencias = ["12345A", "67890B", "54321C", "09876D", "13579E", "24680F", "97531G", "86420H"];
+
+	const nombresCentros = ["Hospital Universitario", "Clínica San Francisco", "Hospital de la Luz", "Hospital Quirón", "Hospital La Paz", "Hospital 12 de Octubre"];
+	const direccionesCentros = ["Calle Mayor", "Plaza del Sol", "Avenida de la Libertad", "Callejón Sin Salida"];
+	//const plantasSalas = [1, 2, 3, 4];
+	//const puertasSalas = ["A", "B", "C", "D"];
+
+	const listaMedicos = [];
+	const listaCentros = [];
+
+
+	for (let i = 0; i < 10; i++) {
+		const nombreAleatorio = nombres[Math.floor(Math.random() * nombres.length)];
+		const apellidoAleatorio = apellidos[Math.floor(Math.random() * apellidos.length)];
+		const licenciaAleatoria = licencias[Math.floor(Math.random() * licencias.length)];
+
+		const medico = new Medico(nombreAleatorio, apellidoAleatorio, licenciaAleatoria);
+
+		listaMedicos.push(medico);
+
+		const nombreCentrosAleatorio = nombresSalas[Math.floor(Math.random() * nombresCentros.length)];
+		const direccionAleatoria = direccionesSalas[Math.floor(Math.random() * direccionesCentros.length)];
+		//const plantaAleatoria = plantasSalas[Math.floor(Math.random() * plantasSalas.length)];
+		//const puertaAleatoria = puertasSalas[Math.floor(Math.random() * puertasSalas.length)];
+
+		const centro = new Centro(nombreCentrosAleatorio, direccionAleatoria);
+		//const sala = new Sala(nombreCentrosAleatorio, direccionAleatoria, plantaAleatoria, puertaAleatoria);
+
+		listaCentros.push(centro);
+	}
+
+	console.log(listaMedicos);
+}
+
 class Centro {
 	constructor(nombre, direccion) {
 		this.nombre = nombre;
@@ -23,7 +63,6 @@ class Sala extends Centro {
 
 class Medico {
 	constructor(nombre, apellidos, licencia) {
-		this.id;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.licencia = licencia;
@@ -46,22 +85,12 @@ class Cita {
 		this.fecha = fecha;
 	}
 
-	addCita() {
-		let usuario = Usuario.addUsuario()
-		let medico = Medico.addMedico()
-		let sala = Sala.addSala()
-		//Date fecha = prompt("Introduce la fecha del usuario:")
-		listaCitas.push(new Cita(usuario, medico, sala))
-	}
+}
 
-	borrarCita() {
-		
-	}
-
-	listarCitas() {
-		for (let i = 0; i < listaCitas.length; i++) {
-			if (listaCitas[i].usuario.dni == document.getElementsByName('usuario')[0].value)
-				console.log(listaCitas[i].usario.nombre)
-		}
-	}
+function crearCita(form) {
+	let usuario = Usuario.addUsuario()
+	let medico = Medico.addMedico()
+	let sala = Sala.addSala()
+	//Date fecha = prompt("Introduce la fecha del usuario:")
+	listaCitas.push(new Cita(usuario, medico, sala))
 }
--- GestionCitasMedicas/src/main/webapp/script/user.js
+++ GestionCitasMedicas/src/main/webapp/script/user.js
@@ -12,13 +12,24 @@ function registrar(form) {
 	let nombre = form.nombre.value
 	let apellidos = form.apellidos.value
 	let dni = form.dniRegistro.value
-	listaUsuarios.push(new Usuario(nombre, apellidos, dni))
+
+	if (nombre != "" && apellidos != "" && dni != "")
+		listaUsuarios.push(new Usuario(nombre, apellidos, dni))
 }
 
 function inicioSesion(form) {
+	for (let i = 0; i < listaUsuarios.length; i++) {
+		if (listaUsuarios[i].dni === form.dniLogin.value) {
+			document.cookie = "nombre=" + listaUsuarios[i].nombre
+			document.cookie = "apellidos=" + listaUsuarios[i].apellidos
+			document.cookie = "dni=" + listaUsuarios[i].dni
+			break;
+		}
+	}
 	if (listaUsuarios.some(usuario => usuario.dni === form.dniLogin.value)) {
 		window.location.href = 'home.html'
 	} else {
 		document.getElementById('error').innerHTML = '[Error] No se ha podido encontrar el usuario'
 	}
 }
+
